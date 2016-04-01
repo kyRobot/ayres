7098ac947a7db67777ae8826c4828cffaaf86bf9
Kyle <kylemilner@me.com>
2016-04-01 23:50:35 +0100

    CLI echos back the args

---
+++ src/main/java/com/kyrobot/ayres/Ayres.java
@@ -0,0 +1,18 @@
+/**
+ * CLI Entry Point for Ayres
+ */
+package com.kyrobot.ayres;
+
+/**
+ * Main provider for Ayres Project
+ */
+public class Ayres {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
